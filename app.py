7b7ca912526f62c42c3027c84242dd03ada8104a
Iman Hedeshy <iman.hedeshy@gmail.com>
2023-11-13 14:26:41 -0500

    add POST request

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def hello_world():
     return "Hello World from Flask!"
+
+@app.route("/submit", methods=["POST"])
+def handle_data():
+    json_data = request.json
+    print(json_data)
+    
+    if json_data:
+        return jsonify({"Received": json_data})
+    else:
+        return jsonify({ "error": "No JSON data received"})
 
 if __name__ == "__main__":
     app.run(debug=True)
