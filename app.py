6bd6827f971dd75e7e8876efde06643f43cd7850
Iman Hedeshy <iman.hedeshy@gmail.com>
2023-11-13 15:00:24 -0500

    update db config

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:Saharkhanoom2#@localhost:3306/first_python"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root#@localhost:3306/first_python"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
