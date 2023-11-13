6717f163c916d881d31558db60c5ba541e97da78
Iman Hedeshy <iman.hedeshy@gmail.com>
2023-11-13 14:34:28 -0500

    create file and save the request body inside

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -12,9 +13,11 @@ def handle_data():
     print(json_data)
     
     if json_data:
-        return jsonify({"Received": json_data})
+        with open("data/data.json", "w") as file:
+            json.dump(json_data, file, indent=4)
+        return jsonify({"data": json_data, "success": "true"})
     else:
-        return jsonify({ "error": "No JSON data received"})
+        return jsonify({ "error": "No JSON data received", "success": "false"})
 
 if __name__ == "__main__":
     app.run(debug=True)
